3c71a5800b2f8e4c2b67d78f456d750b8d21902f
Leila Mirghaderi <leila.mirghaderi@gmail.com>
2019-04-03 18:51:09 -0500

    Find or create Student from auth hash data

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,12 @@
 class SessionsController < ApplicationController
   protect_from_forgery except: :create
   def create
-    @provider_hash = request.env['omniauth.auth'].to_json
+    # @provider_hash = request.env['omniauth.auth'].to_json
+      omni = request.env['omniauth.auth']
+      @author = Student.find_or_create_by(uid: omni['uid']) do |author|
+        author.name = omni['info']['name']
+        author.nickname = omni['info']['nickname']
+        author.email = omni['info']['email']
+      end
   end
 end
