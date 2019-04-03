0f9c7ccd345787d726137f9ca66acd3d69fa12a1
Leila Mirghaderi <leila.mirghaderi@gmail.com>
2019-04-03 18:08:26 -0500

    Generate a Sessions controller with create action
    $ rails g controller Sessions create

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'sessions/create'
   resources :reflections, constraints: { id: /\d+/ }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'reflections#index'
