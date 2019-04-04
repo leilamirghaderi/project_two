6021a481313572d2560022748afcf5fc2b6bf622
Leila Mirghaderi <leila.mirghaderi@gmail.com>
2019-04-03 19:00:15 -0500

    Fix all of the mistakes based on server

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,10 @@ class SessionsController < ApplicationController
   def create
     # @provider_hash = request.env['omniauth.auth'].to_json
       omni = request.env['omniauth.auth']
-      @author = Student.find_or_create_by(uid: omni['uid']) do |author|
-        author.name = omni['info']['name']
-        author.nickname = omni['info']['nickname']
-        author.email = omni['info']['email']
+      @student = Student.find_or_create_by(uid: omni['uid']) do |student|
+        student.firstname = omni['info']['name']
+        student.nickname = omni['info']['nickname']
+        student.email = omni['info']['email']
       end
   end
 end
