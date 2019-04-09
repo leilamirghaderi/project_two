42980ac2843297aa14b9a74cc3cbf22fe50e8b9e
Olivia Dimmer <Olivia.dimmer@gmail.com>
2019-04-09 16:38:19 -0500

    sessions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+  protect_from_forgery except: :create
+  def create
+    # @provider_hash = request.env['omniauth.auth'].to_json
+      omni = request.env['omniauth.auth']
+      @student = Student.find_or_create_by(uid: omni['uid']) do |student|
+        student.firstname = omni['info']['name']
+        student.nickname = omni['info']['nickname']
+        student.email = omni['info']['email']
+      end
+  end
+  def login
+  end
+  def logout
+  end
+end
