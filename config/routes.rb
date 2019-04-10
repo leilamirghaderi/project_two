d0262a0e75e8ed87cf57707ca871652e35552e89
Leila Mirghaderi <leila.mirghaderi@gmail.com>
2019-04-10 16:25:41 -0500

    Change post to reflection

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+  get '/login', to: 'sessions#login', as: 'login'
+  get '/logout', to: 'sessions#login', as: 'logout'
+  get '/auth/github', as: 'github_auth'
+  match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :reflection]
   resources :reflections, constraints: { id: /\d+/ }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'reflections#index'
