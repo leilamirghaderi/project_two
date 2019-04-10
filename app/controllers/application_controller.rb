022e25c9f5d011987417c43fdeebf17cf693ac4b
Leila Mirghaderi <leila.mirghaderi@gmail.com>
2019-04-10 16:15:14 -0500

    Add methods for checking signed-in state

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+  private
+  # Getter method for current User
+  def current_user
+    @current_user |=Student.find_by(uid: session[:uid])
+  end
+  # Setter method for curent user
+  def current_user=(student)
+    session[:uid]=student.uid
+    session[:name]=student.uid
+  end
+
+  def user_signed_in?
+    !!current_user
+  end
+  def authenticate_user
+    unless user_signed_in?
+      redirect_to login_url, notice: "Please log in first"
+    end
+  end
+  
 end
