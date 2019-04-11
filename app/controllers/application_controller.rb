a7a9605e2f17397e8a90a5833cd208920aa41e44
Leila Mirghaderi <leila.mirghaderi@gmail.com>
2019-04-10 19:52:55 -0500

    Add | in front of @current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,12 @@ class ApplicationController < ActionController::Base
   private
   # Getter method for current User
   def current_user
-    @current_user |=Student.find_by(uid: session[:uid])
+    @current_user ||=Student.find_by(uid: session[:uid])
   end
   # Setter method for curent user
   def current_user=(student)
     session[:uid]=student.uid
-    session[:name]=student.uid
+    session[:name]=student.name
   end
 
   def user_signed_in?
@@ -19,5 +19,5 @@ class ApplicationController < ActionController::Base
       redirect_to login_url, notice: "Please log in first"
     end
   end
-  
+
 end
