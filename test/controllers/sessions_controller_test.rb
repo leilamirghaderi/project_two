72c56755be6c458573d43ba49fe1e23c2329323a
Leila Mirghaderi <leila.mirghaderi@gmail.com>
2019-04-20 17:01:06 -0500

    Test that first-time logins create new  records

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,8 +1,13 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
+  test "should redirect to posts URL on login" do
     post '/auth/github/callback', params: OmniAuth.config.mock_auth[:github]
-    assert_response :success	    assert_redirected_to posts_url
+    assert_redirected_to reflections_url
+  end
+  test "first-time login should create a new Student record" do
+    assert_difference('Student.count') do
+      post '/auth/github/callback', params: OmniAuth.config.add_mock(:github, {uid: '12121'})
+    end
   end
 end
