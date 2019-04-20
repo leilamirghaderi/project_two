6d94c676152cc6de1ee23c05c80145d149ef90b3
Leila Mirghaderi <leila.mirghaderi@gmail.com>
2019-04-20 16:20:17 -0500

    Update session#create test

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
   test "should get create" do
-    get sessions_create_url
-    assert_response :success
+    post '/auth/github/callback', params: OmniAuth.config.mock_auth[:github]
+    assert_response :success	    assert_redirected_to posts_url
   end
 end
